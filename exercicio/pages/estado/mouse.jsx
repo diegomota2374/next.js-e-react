2e54f24049f46d449544a535c1dd29b08fe7caf1
diegomota2374 <diegomota2374@gmail.com>
2022-06-12 09:39:16 -0300

    useState usa o estado de uma propriedade

---
+++ exercicio/pages/estado/mouse.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+export default function mouse () {
+    const [x,setX] = useState(0)
+    const [y,setY] = useState(0)
+    
+
+    function quandoMover(ev){
+         setX(ev.clientX)
+         setY(ev.clientY)
+    }
+
+    return(
+        <div style={{
+            display:"flex",
+            flexDirection:"column",
+            justifyContent:"center",
+            alignItems:"center",
+            backgroundColor: "#222",
+            color:"#fff",
+            height:"100vh",
+            }} onMouseMove={quandoMover}>
+            <span>Eixo X: {x}</span>
+            <span>Eixo Y: {y}</span>
+        </div>
+    )
+}
