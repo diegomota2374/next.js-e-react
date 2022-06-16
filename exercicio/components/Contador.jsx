848146191edb037c06c85b278bfea676f25e58df
diegomota2374 <diegomota2374@gmail.com>
2022-06-16 06:44:41 -0300

    novo projeto

---
+++ exercicio/components/Contador.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import Contador from "../pages/classe/contador";
+
+export default class Contador extends Component{
+    render(){
+        return(
+            <Contador/>
+        )
+    }
+}
